7f1662622f7be65b4d7de08d413c7ef3ade54ce8
Daniel J Abraham <dannibla@outlook.com>
2019-07-01 17:55:04 +0530

    updated lib files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+var route = express.Router();
+var baseUrl = '/node/website-pearlhandmades/';
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(baseUrl, express.static(path.join(__dirname, 'public')));
+
+route.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).render('_layout.ejs', {
+        page: 'pages/_home.ejs',
+        base: baseUrl
+    });
+});
+
+app.use(baseUrl, route);
+app.listen(process.env.PORT);
